3935885702076165859be4b6ff71d568ae5f2a8f
lucifron28 <lucifron28@gmail.com>
2025-05-12 18:26:37 +0800

    added event listeners for the timeline select tag

--- static/script.js
+++ static/script.js
@@ -2,3 +2,8 @@ function showSection(section) {
     document.getElementById('artists-section').style.display = section === 'artists' ? 'block' : 'none';
     document.getElementById('tracks-section').style.display = section === 'tracks' ? 'block' : 'none';
 }
+
+document.getElementById('timeline').addEventListener('change', function () {
+    const selectedTimeline = this.value;
+    window.location.href = `?timeline=${selectedTimeline}`;
+})
